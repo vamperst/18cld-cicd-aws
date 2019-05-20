d3522c9bda9999a804f6fdf0444238925432234c
Rafael Barbosa <rafael@rfbarbosa.com>
2019-05-20 19:30:20 -0300

    deploy-ecr

--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "address" {
-  value = "Instances: ${element(aws_instance.web.*.id, 0)}"
-}
-output "elb_public" {
-  value = "${aws_elb.web.dns_name}"
-}
--- terraform/state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "18cld-teste-prod"
-    key    = "test-devops"
-    region = "us-east-1"
-  }
-}
